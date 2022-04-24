96f799f4430dafbace01861882fe471a58e468bb
Doockan <avazyakubow@gmail.com>
2022-04-25 00:26:18 +0400

    AddedManualWork

---
+++ Assets/Scripts/AccelerationMove.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+internal sealed class AccelerationMove : MoveTransform
+{
+    private readonly float _acceleration;
+
+    public AccelerationMove(Transform transform, float speed, float accelerationMove) : base(transform, speed)
+    {
+        _acceleration = accelerationMove;
+    }
+
+    public void AddAcceleration()
+    {
+        Speed += _acceleration;
+    }
+
+    public void RemoveAcceleration()
+    {
+        Speed -= _acceleration;
+    }
+}
+
---
+++ Assets/Scripts/Ship.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+internal sealed class Ship : IMove, IRotation
+{
+    private readonly IMove _moveImplementation;
+    private readonly IRotation _rotationImplementaion;
+    public float Speed => _moveImplementation.Speed;
+
+    public Ship(IMove moveImplementation, IRotation rotationImplementation)
+    {
+        _moveImplementation = moveImplementation;
+        _rotationImplementaion = rotationImplementation;
+    }
+    public void Move(float horizontal, float vertical, float deltaTime)
+    {
+        _moveImplementation.Move(horizontal, vertical, deltaTime);
+    }
+
+    public void Rotation(Vector3 direction)
+    {
+        _rotationImplementaion.Rotation(direction);
+    }
+
+    public void AddAcceleration()
+    {
+        if (_moveImplementation is AccelerationMove accelerationMove)
+        {
+            accelerationMove.AddAcceleration();
+        }
+    }
+
+    public void RemoveAcceleration()
+    {
+        if (_moveImplementation is AccelerationMove accelerationMove)
+        {
+            accelerationMove.RemoveAcceleration();
+        }
+    }
+}
